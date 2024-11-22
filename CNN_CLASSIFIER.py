104bb1481ff578bf1ab734e55266c252f9c923e6
shubh3670 <shubhpatel6001@gmail.com>
2024-11-22 11:05:49 +0530

    Changing The CNN Architecture

--- CNN_CLASSIFIER.py
+++ CNN_CLASSIFIER.py
@@ -1,6 +1,8 @@
 import torch.nn as nn
 import torch.nn.functional as F
 import  torch
+import torch.nn as nn
+import torch.optim as optim
 
 class Net(nn.Module):
     def __init__(self):
@@ -12,6 +14,8 @@ class Net(nn.Module):
         self.fc1=nn.Linear(2560,140)
         self.fc2=nn.Linear(140,90)
         self.fc3=nn.Linear(90,9)
+        self.criterion=None
+        self.optimizer=None
     def forward(self,x):
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool2(F.relu(self.conv2(x)))
@@ -21,4 +25,15 @@ class Net(nn.Module):
         x=self.fc3(x)
 
         return x
+    def Backward(self,net,inputs,label):
+        self.optimizer.zero_grad()
+        outputs = net(inputs)
+        loss = self.criterion(outputs, label)
+        loss.backward()
+        self.optimizer.step()
+
+        return  loss.item()
+    def Temp(self,net):
+        self.criterion=nn.CrossEntropyLoss()
+        self.optimizer=optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 
--- Models.py
+++ Models.py
@@ -4,6 +4,8 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 import torchvision.models as models
+import os
+
 from CNN_CLASSIFIER import  Net
 if __name__ == '__main__':
     from main import DDataset
@@ -23,24 +25,24 @@ if __name__ == '__main__':
         print("Size Of A Batch Is",images.shape)
         break
     net=Net()
+    net.Temp(net)
 
-    criterion = nn.CrossEntropyLoss()
-    optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
     for epoch in range(100):
         running_loss=0.0
         for i,data in enumerate(train_loader):
             inputs,label=data
             batch_size=inputs.shape[0]
             inputs=inputs.view(batch_size, 5 * 3, 214, 214)
-            optimizer.zero_grad()
-            # forward + backward + optimize
-            outputs = net(inputs)
-            loss = criterion(outputs, label)
-            loss.backward()
-            optimizer.step()
 
-            # print statistics
 
-            running_loss += loss.item()
+            running_loss += net.Backward(net,inputs,label,)
             if i==14:
                 print(epoch,running_loss)
+
+    directory = r'C:\Users\91875\OneDrive\Desktop\3D_OBJECT\3DImageReconstruction\SAVED_MODELS'
+    os.makedirs(directory, exist_ok=True)
+
+    PATH = os.path.join(directory, "model.pth")
+
+    torch.save(net.state_dict(), PATH)
