b7c8cfb6c44870f3be265c0a416f290c89d29210
shubh3670 <shubhpatel6001@gmail.com>
2024-11-24 03:58:24 +0530

    Monitoring Validation Loss

--- Models.py
+++ Models.py
@@ -15,10 +15,16 @@ if __name__ == '__main__':
     device=torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(f"Using device: {device}")
 
+
     directory = r'C:\Users\91875\OneDrive\Desktop\3D_RECONSTRUCTION\Training'
     DataFrame = pd.read_csv(r'C:\Users\91875\OneDrive\Desktop\3D_RECONSTRUCTION\Training\Annotations.csv')
     Data = DDataset(DataFrame, directory)
     train_loader = torch.utils.data.DataLoader(Data, batch_size=10, num_workers=2, shuffle=True)
+
+    directory1=r'C:\Users\91875\OneDrive\Desktop\3D_RECONSTRUCTION\Validation'
+    DataFrame1=pd.read_csv(r'C:\Users\91875\OneDrive\Desktop\3D_RECONSTRUCTION\Validation\Annotations.csv')
+    Data1=DDataset(DataFrame1,directory)
+    validation_loader=torch.utils.data.DataLoader(Data1,batch_size=5,num_workers=2,shuffle=True)
 
     # Ensure the code below is only executed after DataLoader is initialized
     train_features, train_labels = next(iter(train_loader))
@@ -35,7 +41,7 @@ if __name__ == '__main__':
     Loss=[]
     Epoch=[]
     Val_Loss=[]
-    for epoch in range(20):
+    for epoch in range(90):
         net.train()
         running_loss=0.0
         for i,data in enumerate(train_loader):
@@ -46,16 +52,30 @@ if __name__ == '__main__':
 
             inputs=inputs.view(batch_size, 5 * 3, 214, 214)
             running_loss += net.Backward(net,inputs,label,)
-        print(f"Epoch:{epoch},Running_Loss:{running_loss}")
+        valid_loss=0.0
+        for i,data in enumerate(validation_loader):
+            inputs, label = data
+            batch_size = inputs.shape[0]
+
+            inputs, label = inputs.to(device), label.to(device)
+            inputs=inputs.view(batch_size, 5 * 3, 214, 214)
+
+            target=net(inputs)
+            valid_loss+=net.criterion(target,label)
+
+
+        print(f"Epoch:{epoch},Running_Loss:{running_loss},Validation_Loss:{valid_loss}")
         Loss.append(running_loss)
+        Val_Loss.append(valid_loss.detach().numpy())
         Epoch.append(epoch)
     fig,axs=plt.subplots(1,2)
-    axs[0].plot(Epoch,Loss)
+    axs[0].plot(Epoch,Loss,color='blue')
     axs[0].set_title("Training Loss Over Epochs")
+    axs[1].plot(Epoch,Val_Loss,color='red')
+    axs[1].set_title("Validation Loss Over Epochs")
     for ax in axs.flat:
         ax.set(xlabel='Epoch',ylabel='Loss')
     plt.show()
-    sys.exit()
     directory = r'C:\Users\91875\OneDrive\Desktop\3D_OBJECT\3DImageReconstruction\SAVED_MODELS'
     os.makedirs(directory, exist_ok=True)
 
